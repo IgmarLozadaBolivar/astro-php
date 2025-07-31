b8450dcad3b22d7fe1d1839a32e8ee8647a4caf6
IgmarLozadaBolivar <losadabolivar@gmail.com>
2025-07-30 19:22:58 -0500

    build: 🔧 introducing tailwindcss and manage commits for console
    download tailwind and c-commit-cli, although tailwind required configuration specify for use

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server : {
+        port: 4323,
+        host: false,
+    },
+    integrations : [
+        tailwind(),
+    ]
+});
